5e1569dbf050eae8dddbd1796e1c2354c85a42c0
Anil Bal <anilbal7676@gmail.com>
2026-03-30 21:53:11 +0545

    preview page add

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router";
 import { DropdownIcon, ExportIcon, PreviewIcon } from "../icons/Icons";
 import { Button, ButtonContainer, NavbarContainer } from "./Navbar.styles";
 import Logo from "./logo/Logo";
@@ -7,9 +8,11 @@ const Navbar: React.FC = () => {
     <NavbarContainer>
       <Logo />
       <ButtonContainer>
+        <Link to="/preview" target="_blank">
         <Button variant="outline">
           <PreviewIcon /> preview
         </Button>
+        </Link>
         <Button variant="solid">
           <ExportIcon /> export <DropdownIcon />
         </Button>
